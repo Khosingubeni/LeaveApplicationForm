93ec72d59d835208f142f51a93b6fe6c4f8d044b
Khosi <khosingubeni@gmail.com>
2024-12-19 14:34:27 +0200

    Add project files.

---
+++ LeaveApplicationForm/Leave Application.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace LeaveApplicationForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fromDate = new System.Windows.Forms.DateTimePicker();
+            this.toLabel = new System.Windows.Forms.Label();
+            this.toDate = new System.Windows.Forms.DateTimePicker();
+            this.label = new System.Windows.Forms.Label();
+            this.reasonsRichTxb = new System.Windows.Forms.RichTextBox();
+            this.applyBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.leavetypeCmbx = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.namesTxb = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LeaveApplicationForm.Properties.Resources.Santam_Logo_1_e1573717441852;
+            this.pictureBox1.Location = new System.Drawing.Point(33, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(341, 124);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "From";
+            // 
+            // fromDate
+            // 
+            this.fromDate.Location = new System.Drawing.Point(155, 228);
+            this.fromDate.Name = "fromDate";
+            this.fromDate.Size = new System.Drawing.Size(200, 20);
+            this.fromDate.TabIndex = 2;
+            // 
+            // toLabel
+            // 
+            this.toLabel.AutoSize = true;
+            this.toLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toLabel.Location = new System.Drawing.Point(45, 271);
+            this.toLabel.Name = "toLabel";
+            this.toLabel.Size = new System.Drawing.Size(26, 16);
+            this.toLabel.TabIndex = 1;
+            this.toLabel.Text = "To";
+            // 
+            // toDate
+            // 
+            this.toDate.Location = new System.Drawing.Point(155, 271);
+            this.toDate.Name = "toDate";
+            this.toDate.Size = new System.Drawing.Size(200, 20);
+            this.toDate.TabIndex = 2;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(45, 304);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(69, 16);
+            this.label.TabIndex = 1;
+            this.label.Text = "Reasons";
+            // 
+            // reasonsRichTxb
+            // 
+            this.reasonsRichTxb.Location = new System.Drawing.Point(155, 304);
+            this.reasonsRichTxb.Name = "reasonsRichTxb";
+            this.reasonsRichTxb.Size = new System.Drawing.Size(200, 96);
+            this.reasonsRichTxb.TabIndex = 3;
+            this.reasonsRichTxb.Text = "";
+            // 
+            // applyBtn
+            // 
+            this.applyBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.applyBtn.Location = new System.Drawing.Point(33, 406);
+            this.applyBtn.Name = "applyBtn";
+            this.applyBtn.Size = new System.Drawing.Size(322, 28);
+            this.applyBtn.TabIndex = 4;
+            this.applyBtn.Text = "Apply";
+            this.applyBtn.UseVisualStyleBackColor = true;
+            this.applyBtn.Click += new System.EventHandler(this.applyBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearBtn.Location = new System.Drawing.Point(33, 454);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(322, 26);
+            this.clearBtn.TabIndex = 4;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Leave Type";
+            // 
+            // leavetypeCmbx
+            // 
+            this.leavetypeCmbx.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.leavetypeCmbx.FormattingEnabled = true;
+            this.leavetypeCmbx.Items.AddRange(new object[] {
+            "Annual Leave",
+            "Sick Leave",
+            "Maternity Leave",
+            "Paternity Leave",
+            "Family Responsibiity",
+            "Study Leave"});
+            this.leavetypeCmbx.Location = new System.Drawing.Point(156, 191);
+            this.leavetypeCmbx.Name = "leavetypeCmbx";
+            this.leavetypeCmbx.Size = new System.Drawing.Size(200, 21);
+            this.leavetypeCmbx.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(45, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Full Names";
+            // 
+            // namesTxb
+            // 
+            this.namesTxb.Location = new System.Drawing.Point(156, 147);
+            this.namesTxb.Name = "namesTxb";
+            this.namesTxb.Size = new System.Drawing.Size(200, 20);
+            this.namesTxb.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(403, 492);
+            this.Controls.Add(this.namesTxb);
+            this.Controls.Add(this.leavetypeCmbx);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.applyBtn);
+            this.Controls.Add(this.reasonsRichTxb);
+            this.Controls.Add(this.toDate);
+            this.Controls.Add(this.fromDate);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.toLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Leave Application";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker fromDate;
+        private System.Windows.Forms.Label toLabel;
+        private System.Windows.Forms.DateTimePicker toDate;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.RichTextBox reasonsRichTxb;
+        private System.Windows.Forms.Button applyBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox leavetypeCmbx;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox namesTxb;
+    }
+}
+
---
+++ LeaveApplicationForm/Leave Application.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeaveApplicationForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            resetData();
+        }
+
+        private void resetData()
+        {
+            namesTxb.Text = "";
+            reasonsRichTxb.Text = "";
+            leavetypeCmbx.SelectedIndex = -1;
+            fromDate.Value = DateTime.Now;
+            toDate.Value = DateTime.Now;
+        }
+
+        private void applyBtn_Click(object sender, EventArgs e)
+        {
+            if(fromDate.Value <= toDate.Value && namesTxb.Text.Length > 0 && leavetypeCmbx.SelectedIndex != -1 && reasonsRichTxb.Text.Length > 0)
+            {
+                MessageBox.Show("Leave Application Successful");
+                resetData();
+            }
+
+            else
+            {
+                MessageBox.Show(" Full Names Not Provided" + "\n Leave Type Not Selected" + "\n Reasons Not Stated");
+            }
+        }
+    }
+}
